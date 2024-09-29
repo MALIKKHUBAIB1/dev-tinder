02e5bf581aef87b039ea138aee436a9a2072540a
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2024-09-29 17:57:34 +0530

    setup project

---
+++ Backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+});
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
