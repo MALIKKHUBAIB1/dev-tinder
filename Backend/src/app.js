7f79813a8f6d719a7128d2afe38f8e6036d8bd62
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2024-10-13 23:56:27 +0530

    handle user request dynamically

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,16 +4,35 @@ const { connectToDataBase } = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const userData = {
-    firstName: "Jhon",
-    lastName: "Doe",
-    age: 34,
-    gender: "Male",
-    password: "password",
-    email: "email@example.com",
-  };
   try {
+    const userData = req.body;
+    const { firstName, lastName, age, gender, password, email } = userData;
+    const isDataValid = [
+      firstName,
+      lastName,
+      age,
+      gender,
+      password,
+      email,
+    ].some((isValid) => {
+      if (!isValid) return true;
+    });
+    // console.log(userData);
+
+    console.log(isDataValid);
+    if (isDataValid) {
+      res.status(400).send("please enter a valid  address feild");
+      return;
+    }
+    const findUser = await User.findOne({ $or: [{ email }, { password }] });
+    if (findUser) {
+      res.status(401).send("user already exists");
+      return;
+    }
     const user = await new User(userData);
     user.save();
     res.send("user created successfully");
