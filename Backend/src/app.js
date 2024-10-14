76b6eced2ab8150ae0a82aad0de26b149133bfea
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2024-10-14 21:48:56 +0530

    Api validation and sanitization and putting some checks

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,43 +1,121 @@
 const express = require("express");
 const { isUserAuthenticated } = require("../utils/middleware/auth");
 const { connectToDataBase } = require("./config/database");
+var validator = require("validator");
+
 const User = require("./models/user");
 const app = express();
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+//singup user
 app.post("/signup", async (req, res) => {
   try {
     const userData = req.body;
     const { firstName, lastName, age, gender, password, email } = userData;
-    const isDataValid = [
-      firstName,
-      lastName,
-      age,
-      gender,
-      password,
-      email,
-    ].some((isValid) => {
-      if (!isValid) return true;
-    });
-    // console.log(userData);
-
-    console.log(isDataValid);
-    if (isDataValid) {
-      res.status(400).send("please enter a valid  address feild");
-      return;
+    if (![firstName, lastName, age, gender, password, email].every(Boolean)) {
+      return res
+        .status(404)
+        .send("invalid feild please fill the feild with valid values");
     }
-    const findUser = await User.findOne({ $or: [{ email }, { password }] });
+    if (!["male", "female", "others"].includes(gender)) {
+      return res.status(401).send("invalid gender feild");
+    }
+    const findUser = await User.findOne({ email });
     if (findUser) {
-      res.status(401).send("user already exists");
-      return;
+      return res.status(401).send("user already exists");
     }
-    const user = await new User(userData);
+    const user = new User(userData);
     user.save();
-    res.send("user created successfully");
+    res.status(200).send("user created successfully");
   } catch (error) {
     res.status(400).send("error while saving the user " + error.message);
+  }
+});
+// get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(404).send("user not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("something went wrong" + error.message);
+  }
+});
+//get all the user
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("user not found");
+    }
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("something went wrong" + error.message);
+  }
+});
+// delete the usr by email
+app.delete("/user", async (req, res) => {
+  try {
+    const email = req.body.email;
+    if (!email) {
+      res.status(404).send("please enter a valid email");
+    } else {
+      const user = await User.findOneAndDelete({ email });
+      if (!user) {
+        res.status(404).send("user not found");
+      }
+      console.log(user);
+      res.status(200).send(user.email + " deleted successfully");
+    }
+  } catch (error) {
+    res.status(401).send(error.message || "something went wrong");
+  }
+});
+
+// update the user by email or id
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+
+    if (!id) {
+      return res.status(401).send("enter a valid id ");
+    }
+
+    const ALLOWED_UPDATES = [
+      "id",
+      "gender",
+      "skills",
+      "age",
+      "firstName",
+      "lastName",
+      "photoUrl",
+    ];
+    const isAllowedValid = Object.keys(data).every((key) =>
+      ALLOWED_UPDATES.includes(key)
+    );
+    if (req.body?.skills.length > 10) {
+      return res.status(401).send("the skill length should less then 10");
+    }
+    if (!isAllowedValid) {
+      return res.status(401).send("you can not update this feild");
+    }
+
+    const user = await User.findByIdAndUpdate(id, data, {
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send("user not found");
+    }
+    res.status(200).send("user updated successfully");
+  } catch (error) {
+    res.status(401).send(error.message + " some error");
   }
 });
 connectToDataBase()
--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,27 +1,69 @@
 const mongoose = require("mongoose");
+var validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 16,
+      trim: true,
     },
     lastName: {
       type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 16,
+      trim: true,
     },
     email: {
       type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (validator.isEmail(value)) {
+          throw new Error(" email id is not valid");
+        }
+      },
     },
     password: {
       type: String,
+      required: true,
+      trim: true,
     },
     age: {
       type: Number,
+      trim: true,
     },
     gender: {
       type: String,
+      validate: {
+        validator: function (value) {
+          // Check if gender is one of the allowed values
+          return ["male", "female", "others"].includes(value);
+        },
+        message: (props) =>
+          `${props.value} is not a valid gender. Allowed values are 'male', 'female', or 'others'.`,
+      },
+      required: [true, "Gender is required"],
+      default: "others",
+    },
+    photoUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/219/219988.png",
+    },
+    skills: {
+      type: [String],
+    },
+    about: {
+      type: String,
+      default: "this is about user bio ",
+      trim: true,
     },
   },
-  { Timestamp: true }
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
